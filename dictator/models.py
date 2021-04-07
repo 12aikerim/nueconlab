c5816bd8b614c6ae8d86381af3bc2d9a178ee6d4
12aikerim <aikerim.orken@gmail.com>
2021-04-07 20:33:17 +0600

    Rolling dice game 1 and Dictator game

---
+++ dictator/models.py
@@ -0,0 +1,47 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'dictator'
+    players_per_group = None
+    num_rounds = 1
+    endowment = c(3000)
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+
+    add = models.IntegerField(
+        choices=[[1, "Add to Receiver's earnings"], [0, "Subtract from Receiver's endowment"]],
+        label='Choose the action you want to do',
+        widget=widgets.RadioSelect,
+    )
+    amount = models.IntegerField(
+        min=0,
+        max=Constants.endowment,
+        label='How much you want to add (or subtract)? ',
+        doc='Amount to be added/subtracted'
+    )
+    pass
